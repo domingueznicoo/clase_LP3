3893b06a67376dd139e456f41c305b7e5d1f5867
domingueznicoo <nicolasdomimguez1808004@gmail.com>
2024-10-01 19:43:27 -0400

    primer commit

---
+++ 8.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Contenedores - div</title>
+
+        <style>
+            .ciudad{
+                background-color: gray;
+                color: green;
+                margin: 20px;
+                border-bottom: 2px solid blue;
+
+                padding: 20px;
+            }
+
+            #titulo{
+                background-color: red;
+                color: gray;
+                padding: 15px;
+                text-align: center;
+                text-decoration: darkblue;
+            }
+        </style>
+    </head>
+    <body>
+        <h1 id="titulo">Estas son las capitales de esos paises</h1>
+        <div class="ciudad">
+            <h1>Asunción</h1>
+            <p>Asunción, es la capital de Paraguay</p>
+        </div>
+
+        <hr>
+        <p id="titulo">este es un parrafo con estilo</p>
+        <div class="ciudad">
+            <h1>Montevideo</h1>
+            <p>Montevideo, es la capital de Uruguay</p>
+        </div>
+    </body>
+</html>
